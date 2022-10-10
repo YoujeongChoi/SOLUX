28e26e64e169edd27661e14735ecde77e971cbd1
YoujeongChoi <>
2022-10-10 22:41:05 +0900

    message

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ app.get('/', (req, res) => {
     res.send('Hello home page');
 });
 
+app.get('/dynamic', (req, res)=> {
+    var lis = '';
+    for(var i=0; i<5; i++){
+        lis = lis + '<li>coding</li>'
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        Hello Dynamic!
+        <ul>${lis}</ul>
+        ${time}
+    </body>
+    </html>`
+    res.send(output);
+})
+
 app.get('/route',(req, res) => {
     res.send('Hello Router, <img src="/route.jpg">');
 })
